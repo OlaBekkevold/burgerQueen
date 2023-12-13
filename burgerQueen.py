867728c266eeedea8d0642fba7715dc86fa1329e
OlaBekkevold <ola.k.bekkevold@gmail.com>
2023-12-13 19:15:24 +0100

    Make login and register less complicated
    Make both login and register a single function to make the login process faster

--- burgerQueen.py
+++ burgerQueen.py
@@ -2,74 +2,64 @@ import sqlite3
 
 con = sqlite3.connect('burgerQueen.db')
 cursor = con.cursor()
-
-def register():
-    print("Enter your details below to register a new account")
-    print("")
-    username = input("Enter your username: ").strip()
-    password = input("Enter your password: ").strip()
-    role = int(input("Are you employed at Burger Queen? (1 for yes, 0 for no): "))
-    if not username or not password:
-        print("A username and password is required")
-        
-    elif role not in [0, 1]:
-        print("Please enter a valid role")
-        
-    else:
-         cursor.execute("INSERT INTO person (Brukernavn, Passord, Ansatt) VALUES (?, ?, ?)", (username, password, role))
-         con.commit()
          
-
 def login():
-     print("Enter your account information to login\n")
-     print("")
+     
+     loginOption = input("1. Login\n2. Register\n")
 
-     username = input("Enter your username: ").strip()
-     password = input("Enter your password: ").strip()
-     role = int(input("Are you employed at Burger Queen? (1 for yes, 0 for no): "))
+     if loginOption == "1":
 
-     cursor.execute("SELECT * FROM person WHERE Brukernavn = ? AND Passord = ? AND Ansatt = ?", (username, password, role))
-     user = cursor.fetchone()
+        print("Enter your account information to login\n")
+        username = input("Enter your username: ").strip()
+        password = input("Enter your password: ").strip()
+        role = int(input("Are you employed at Burger Queen? (1 for yes, 0 for no): "))
 
-     if user is None:
-          print("Incorrect information, please try again")
+        cursor.execute("SELECT * FROM person WHERE Brukernavn = ? AND Passord = ? AND Ansatt = ?", (username, password, role))
+        user = cursor.fetchone()
+
+        if user is None:
+            print("Incorrect information, please try again")
+
+        else:
+            print("Logged in successfully")
+            return username, password, role
+        
+     elif loginOption == "2":
+
+        print("Enter your details below to register a new account\n")
+        
+        username = input("Enter your username: ").strip()
+        password = input("Enter your password: ").strip()
+        role = int(input("Are you employed at Burger Queen? (1 for yes, 0 for no): "))
+
+        if not username or not password:
+            print("A username and password is required")
+            
+        elif role not in [0, 1]:
+            print("Please enter a valid role")
+            
+        else:
+            cursor.execute("INSERT INTO person (Brukernavn, Passord, Ansatt) VALUES (?, ?, ?)", (username, password, role))
+            con.commit()
+            print("Account created successfully")
+            return username, password, role
 
      else:
-          print("Logged in successfully")
-          return username, password, role
+        print("Please enter a valid option")
+
+
 
 def customerMenu(username, password, role):
      print("Welcome to Burger Queen " + username + "!")
 
 
-
-
-
-print("Welcome to Burger Queen!")
-print("")
-print("+++++++++++++++++++++++++")
-print("+++++++++++++++++++++++++")
-print("")
-
-
-
 def main():
     while True:
-        startOption = input("1. Login\n2. Register")
-        if role == 0:
-          customerMenu(username, password, role)       
-        elif startOption == "1":
-             username, password, role = login()
-             
-        elif startOption == "2":
-             register()
-        else:
-              print("Please enter a valid option")
-
-    
+        if 'role' not in locals():
+            username, password, role = login()
+        elif role == 0:
+            customerMenu(username, password, role)
 
 
 if __name__ == "__main__":
     main()
-
-
